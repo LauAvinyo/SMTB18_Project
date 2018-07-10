b416464003f93fd55dec044de0415fc4e093b519
Laura Avinyó Esteban <avinyo.laura@gmail.com>
2018-07-10 13:10:23 +0200

    Update and rename 03.transformSequences.py to clustal2phylip.py

--- Scripts/clustal2phylip.py
+++ Scripts/clustal2phylip.py
@@ -1,17 +1,19 @@
 from Bio import AlignIO
 import sys
 
+# It recieves a list of pritein aligments 
+# It changes the format of the the aligment
 prots = sys.argv[1:]
 for input_name in prots:
     print(input_name)
-    output_name = input_name[:-3]+'rel'
+    output_name = input_name[:-3]+'phy'
     print(output_name)
 
     input_handle = open(input_name, "rU")
     output_handle = open(output_name, "w")
 
     alignments = AlignIO.parse(input_handle, "clustal")
-    AlignIO.write(alignments, output_handle, "phylip-relaxed")
+    AlignIO.write(alignments, output_handle, "phylip")
 
     output_handle.close()
     input_handle.close()

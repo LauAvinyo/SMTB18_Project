b765a6bcfa2206d0b64e8862e5cd7fc2b213788f
LauAvinyo <avinyo.laura@gmail.com>
2018-08-12 14:38:33 +0200

    tones of stuff

--- Scripts/concatenate.sh
+++ Scripts/concatenate.sh
@@ -3,7 +3,7 @@ for f in *.fasta; do
   echo "$o"                             # prints the code
   mkdir "$o"                            # makes a directory with the name of the code
   cd "$o"                               # move to new directory
-  python3 /mnt/c/Users/laura/SMTB18/SMTB18_Project/Scripts/03.getHomologous.py ../"$o"".blast"  # python script to retreive all seq
+  python3 /home/a/SMTB18_Project/Scripts/03.getHomologous.py ../"$o"".blast"  # python script to retreive all seq
   cat *.fasta > "$o"".fst"              # concatenate everything
   rm *.fasta                            # remove single fastas
   cd ..
